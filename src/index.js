64ef6ab1c70e8adc6cb353397939d1b5daa99855
Emily Wu <huijiawu90@gmail.com>
2020-05-01 12:35:55 -0400

    Add how to play modal

--- src/index.js
+++ src/index.js
@@ -10,4 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener('click', e => {
         game.restart();
     });
+
+    const modal = document.getElementById('modal');
+    const trigger = document.getElementById('modal-trigger');
+    trigger.addEventListener('click', e=> {
+        e.preventDefault();
+        modal.classList.add('show');
+    })
+
+    const closeButton = document.getElementById('close-button');
+    closeButton.addEventListener('click', e => {
+        e.preventDefault();
+        modal.classList.remove('show');
+    });
+
+    window.addEventListener('click', e => {
+        if (e.target.classList.contains('show')) {
+            modal.classList.remove('show');
+        }
+    });
 });
